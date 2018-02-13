7297967a53fca97f6c98a1d8840472fe8acc2117
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-02-13 12:19:02 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+
+//Persistent data structures for efficient immutablity
+
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "ballroom";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room") {
+    weapon = "knife";
+    solved = true;
+    suspect = "Mr. Parkes";
+} else if (room === "gallery") {
+    weapon = "trophy"
+    solved = true;
+    suspect = "Ms. Van Cleve";
+} else if (room == "ballroom") {
+    weapon = "poison";
+    solved = true;
+    suspect = "Mr. Kalehoff";
+} else {
+    weapon = "pool stick";
+    solved = true;
+    suspect = "Mrs. Sparr";
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+
+
+/*
+Call the emotions() function so that it prints the output you see below,
+but instead of passing the laugh() function as an argument, pass an inline function expression instead.
+*/
+
+//Prints: "I am happy, haha!"
+
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// call the emotions function here and pass in an
+// inline function expression
+emotions('happy', function(num){
+    var message = "";
+    for(var i = 0; i < num; i++){
+        message += "ha";
+    }
+    return message + "!";
+})
